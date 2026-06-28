ff40d541a769e59df28631445b83ee3cf50722f4
jinesh Jain <jinujain40@gmail.com>
2026-06-28 23:20:07 +0530

    unknown

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:financ_app/Screens/Splash_Screen.dart';
-import 'package:financ_app/widgets/Budget_Card.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -36,7 +35,7 @@ class _MyAppState extends State<MyApp> {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Color(0xffF5F6FA)),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xffF5F6FA)),
       ),
       home: SplashScreen(),
     );
@@ -111,7 +110,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
           // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
-          mainAxisAlignment: .center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text('You have pushed the button this many times:'),
             Text(
